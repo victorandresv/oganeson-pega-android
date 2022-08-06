d03bfc50ada5c5e1ee9a3894937f56e6afc9e20b
Victor Vargas <victor.vargas@agentcars.com>
2022-08-06 11:56:52 -0300

    Firebase firestore products connected

--- app/src/main/java/br/pega/oganeson/ProductList.java
+++ app/src/main/java/br/pega/oganeson/ProductList.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class ProductList extends AppCompatActivity {
 
@@ -18,6 +22,17 @@ public class ProductList extends AppCompatActivity {
             startActivity(new Intent(this, MainActivity.class));
         } else {
             setContentView(R.layout.activity_product_list);
+
+            FirebaseFirestore.getInstance()
+                    .collection("products")
+                    .get()
+                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                        @Override
+                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                            Log.e("KIK", queryDocumentSnapshots.size()+"<");
+                        }
+                    });
+
         }
     }
 }
