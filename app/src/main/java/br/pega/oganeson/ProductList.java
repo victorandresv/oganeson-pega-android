2df2ef1c30c52d2ae2d8006916902db4bd029efe
Victor Vargas <victor.vargas@agentcars.com>
2022-08-06 13:54:38 -0300

    Item list product

--- app/src/main/java/br/pega/oganeson/ProductList.java
+++ app/src/main/java/br/pega/oganeson/ProductList.java
@@ -1,21 +1,30 @@
 package br.pega.oganeson;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
-import com.google.android.gms.tasks.OnSuccessListener;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+import br.pega.oganeson.adapters.ProductListAdapter;
+import br.pega.oganeson.models.Product;
 
 public class ProductList extends AppCompatActivity {
+
+    private ArrayList<Product> data;
+    private ProductListAdapter productListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        data = new ArrayList<>();
 
         if (FirebaseAuth.getInstance().getCurrentUser() == null){
             finish();
@@ -23,16 +32,43 @@ public class ProductList extends AppCompatActivity {
         } else {
             setContentView(R.layout.activity_product_list);
 
+            RecyclerView list = findViewById(R.id.list);
+            productListAdapter = new ProductListAdapter(data);
+            list.setAdapter(productListAdapter);
+
+
             FirebaseFirestore.getInstance()
                     .collection("products")
                     .get()
-                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                        @Override
-                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                            Log.e("KIK", queryDocumentSnapshots.size()+"<");
+                    .addOnSuccessListener(queryDocumentSnapshots -> {
+                        if (queryDocumentSnapshots.size() > 0){
+                            for(DocumentSnapshot item: queryDocumentSnapshots.getDocuments()){
+                                SetProduct(item);
+                            }
                         }
                     });
 
+
         }
+    }
+
+    private void SetProduct(DocumentSnapshot item){
+        Product product = new Product();
+        product.setEnabled(true);
+        product.setFirebaseId(item.getId());
+        product.setSku(item.getData().get("sku").toString());
+        product.setName(item.getData().get("name").toString());
+
+        FirebaseFirestore.getInstance()
+                .collection("products")
+                .document(item.getId())
+                .collection("thumbnail")
+                .document("image")
+                .get()
+                .addOnSuccessListener(snap ->{
+                    product.setThumbnailSmall(snap.getData().get("data").toString());
+                    data.add(product);
+                    productListAdapter.notifyDataSetChanged();
+                });
     }
 }
--- app/src/main/java/br/pega/oganeson/adapters/ProductListAdapter.java
+++ app/src/main/java/br/pega/oganeson/adapters/ProductListAdapter.java
@@ -1,2 +1,77 @@
-package br.pega.oganeson.adapters;public class ProductListAdapter {
+package br.pega.oganeson.adapters;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import br.pega.oganeson.R;
+import br.pega.oganeson.models.Product;
+
+public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.ViewHolder>{
+
+    private final ArrayList<Product> data;
+
+    public ProductListAdapter(ArrayList<Product> data) {
+        this.data = data;
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        viewHolder.getSku().setText(data.get(position).getSku());
+        viewHolder.getName().setText(data.get(position).getName());
+
+        String imageString = data.get(position).getThumbnailSmall();
+        byte[] decodedString = Base64.decode(imageString, Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        viewHolder.getImage().setImageBitmap(decodedByte);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.product_item, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView sku;
+        private final TextView name;
+        private final ImageView image;
+
+        public ViewHolder(View view) {
+            super(view);
+            sku = view.findViewById(R.id.sku);
+            name = view.findViewById(R.id.name);
+            image = view.findViewById(R.id.image);
+        }
+
+        public TextView getSku() {
+            return sku;
+        }
+
+        public TextView getName() {
+            return name;
+        }
+
+        public ImageView getImage() {
+            return image;
+        }
+    }
 }
